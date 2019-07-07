4421947d478879f698b644a74a6e113fb2113ebf
Nisrine Zerouali <nisszerouali@gmail.com>
2019-07-07 12:32:12 +0100

    Find first unique character in a string problem.

---
+++ first_unique_ch.py
@@ -0,0 +1,22 @@
+#link: https://leetcode.com/problems/first-unique-character-in-a-string/
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count_letter = dict()
+        if s == "":
+            return -1
+        
+        for letter in list(s): 
+            if letter in count_letter:
+                count_letter[letter] += 1
+            else:
+                count_letter[letter] = 1
+
+        for i, letter in enumerate(list(s)):
+            if letter in count_letter and count_letter[letter] == 1:
+                return(i)
+        return -1
+            
