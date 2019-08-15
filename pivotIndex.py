cdc3b17bd3ceb8236b5e5e559ad7b6aa4294261c
Nisrine Zerouali <nisszerouali@gmail.com>
2019-08-15 12:19:57 +0100

    Find pivot index where sum of the nums to the left of the index is equal to the ones on the right.

---
+++ pivotIndex.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        sum_nums = sum(nums) 
+        left = 0
+        #right = sum_nums - left - num
+        for i, num in enumerate(nums):
+            if left == (sum_nums - left - num):
+                return i
+            left += num
+        return -1
