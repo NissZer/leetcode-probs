9cbc173c42f6a86d09042791bd1b78b0dc7014b5
Nisrine Zerouali <nisszerouali@gmail.com>
2019-08-16 18:17:08 +0100

    Check if two rectangles overlap given a list of their coordinates.

---
+++ rect_overlap.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/rectangle-overlap/
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        x = min(rec1[2], rec2[2]) - max(rec1[0], rec2[0])
+        y = min(rec1[3], rec2[3]) - max(rec1[1], rec2[1])
+
+        if x > 0 and y > 0:
+            return True
+        else:
+            return False
